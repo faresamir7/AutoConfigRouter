c2aec3cddf46441098fd75a48af08a2b9926671d
Fares <faresamir7@gmail.com>
2022-04-05 11:18:27 +0100

    Start of Cisco Scripting

--- AutoConfigRouter/autoconfigrouter.py
+++ AutoConfigRouter/autoconfigrouter.py
@@ -1,12 +1,35 @@
 from asyncio.windows_events import NULL
+import json
 from logging import exception
+from urllib import request
 from fireREST import FMC
 
-from AutoConfigRouter import CONNECTION_ERROR
+from AutoConfigRouter import CONNECTION_ERROR, CREDENTIAL_ERROR, SESSION_ERROR
 
-fmc = FMC("192.168.1.1","cisco","cisco","Global")
+#fmc = FMC("192.168.1.1","cisco","cisco","Global")
+client = request.session()
 
-def getRule():
+def addRulesCisco(ruleset, login, passwd, ip_addr):
+    fmc = FMC(ip_addr,login,passwd,"Global")
+    for x in ruleset:
+        rule = {
+            'interface': x[0],
+            'action': x[1],
+            'source': x[2],
+            'user': x[3],
+            'securitygroup': x[4],
+            'destination' : x[5],
+            'securitygroup': x[6],
+            'service': x[7],
+            'description': x[8],
+            'logging': x[9],
+            'logginglevel': x[10],
+        }
+        addRuleCisco(rule)
+
+
+
+def getRuleCisco(fmc):
     try:
         if(fmc!=NULL):
             ruleset = fmc.policy.accesspolicy.get()
@@ -17,7 +40,7 @@ def getRule():
         return CONNECTION_ERROR
             
 
-def addRule(rule):
+def addRuleCisco(rule,fmc):
     try:
         if(fmc!=NULL):
             print(fmc.policy.accesspolicy.create(rule))
@@ -25,3 +48,18 @@ def addRule(rule):
             raise Exception
     except Exception:
         return CONNECTION_ERROR
+
+
+def getRulesFortigate(headers, url, customerID):
+    try:
+        #Policies request
+        url_cust_req=url+"/customers/"+customerID+"/policyobjects"
+        r = client.get(url_cust_req, headers=headers, verify=False)
+        
+        if(r == NULL):
+            raise Exception
+        else:
+            print(r)
+    except Exception:
+        return SESSION_ERROR
+    
--- AutoConfigRouter/cli.py
+++ AutoConfigRouter/cli.py
@@ -7,7 +7,7 @@ from openpyxl import load_workbook
 
 import openpyxl
 import typer
-import autoconfigrouter
+import AutoConfigRouter.autoconfigrouter
 
 
 from AutoConfigRouter import __app_name__, __version__, ERRORS, config
@@ -15,25 +15,45 @@ from AutoConfigRouter import __app_name__, __version__, ERRORS, config
 app = typer.Typer()
 
 @app.command()
-def init(
+def upload(
     ip_address: str = typer.Option(
         "192.168.1.1",
         "--ip-address",
         "-ip",
         prompt="Set ip address:",
     ),
+    login: str = typer.Option(
+        "admin",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "password",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+    file: str = typer.Option(
+        r"C:\Users\User\Documents\spreadsheet.xlsx",
+        "--file",
+        "-f",
+        prompt="Set file location:",
+    ),
 ) -> None:
     """Initialize the connection phase."""
-    app_init_error = config.init_app()
-    if app_init_error:
-        typer.secho(
-            f'Creating config file failed with "{ERRORS[app_init_error]}"',
-            fg=typer.colors.RED,
-        )
-        raise typer.Exit(1)
+    wb = openpyxl.Workbook()
+    wb = load_workbook(filename = file, read_only=1)
+    ws = wb.active
+    w, h = ws.max_row, ws.max_column
+    ruleset = [[0 for x in range(w)] for y in range(h)]
+    for i in range(2, ws.max_row+1):
+        for j in range(1, ws.max_column+1):
+            ruleset[i][j] = ws.cell(row=i, column=j)            
+    AutoConfigRouter.autoconfigrouter.addRulesCisco(ruleset,login,password,ip_address)
 
 @app.command()
-def load(
+def validate(
     document_path: str = typer.Option(
         r"C:\Users\User\Documents\spreadsheet.xlsx",
         "--document",
