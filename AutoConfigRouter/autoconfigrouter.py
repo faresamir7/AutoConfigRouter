f9bc12d39689779cfb986dc7aee83124d171f143
Fares <faresamir7@gmail.com>
2022-05-30 08:33:05 +0100

    Final touches on Cisco

--- AutoConfigRouter/autoconfigrouter.py
+++ AutoConfigRouter/autoconfigrouter.py
@@ -1,43 +1,163 @@
+from http import cookiejar
 from logging import exception
+from operator import indexOf
+from textwrap import indent
 from fireREST import FMC
+from fireREST import exceptions
+from pyfortiapi import FortiGate
 from AutoConfigRouter import CONNECTION_ERROR, CREDENTIAL_ERROR, SESSION_ERROR
 import requests
+import json
 
 #client = request.session()
 
-def addRulesCisco(ruleset, login, passwd, ip_addr):
-    fmc = FMC(ip_addr,login,passwd,"Global")
+def addRulesCisco(ruleset, login, passwd, ip_addr, containerName):
+    fmc = FMC(ip_addr,login,passwd,"https")
     for x in ruleset:
-        rule = {
-            'interface': x[0],
-            'action': x[1],
-            'source': x[2],
-            'user': x[3],
-            'securitygroup': x[4],
-            'destination' : x[5],
-            'securitygroup': x[6],
-            'service': x[7],
-            'description': x[8],
-            'logging': x[9],
-            'logginglevel': x[10],
-        }
-        fmc.policy.accesspolicy.create(data=rule)
+        rule = {'id': [x[0]],
+                "vlanTags": {x[1]},
+                "logBegin": x[2],
+                "logEnd": x[3],
+                "logFiles": x[4],
+                "sendEventsToFMC": x[5],
+                "type": "AccessRule",
+                "action": x[6],
+                "name": x[7],
+                "enabled": x[8],
+                "sourceNetworks": x[9],
+                "destinationNetworks": x[10],
+                "sourcePorts": x[11],
+                "destinationPorts": x[12]
+                }
+        if(x[0]=="None"):
+            rule.pop("id")
+        if(x[1]=="None"):
+            rule.pop("vlanTags")
+        if(x[2]=="None"):
+            rule.pop("logBegin")
+        if(x[3]=="None"):
+            rule.pop("logEnd")
+        if(x[4]=="None"):
+            rule.pop("logFiles")
+        if(x[5]=="None"):
+            rule.pop("sendEventsToFMC")
+        if(x[6]=="None"):
+            rule.pop("action")
+        if(x[7]=="None"):
+            rule.pop("name")
+        if(x[8]=="None"):
+            rule.pop("enabled")
+        if(x[9]=="None"):
+            rule.pop("sourceZones")
+        if(x[10]=="None"):
+            rule.pop("destinationZones")
+        if(x[11]=="None"):
+            rule.pop("sourcePorts")
+        if(x[12]=="None"):
+            rule.pop("destinationPorts")
+        networks = fmc.object.network.get()
+        for i in x[9]:
+            if((not(i in networks.values()))):
+                print("Source Network not found...")
+                addNetObjCisco(fmc,i)
+            if((not(i in networks.values()))):
+                print("Destination Network not found...")
+                addNetObjCisco(fmc,i)
+        fmc.policy.accesspolicy.accessrule.create(data=rule,container_name=containerName)
+
+
+def addZoneCisco(fmc: FMC, name):
+    print("Adding Zone with the name: "+ name)
+    print("Description: ")
+    desc = str(input())
+    r=1
+    while(r>0):
+        print("Interface: ")
+        inter = str(input())
+        if(fmc.object.interface.get(name=inter)==[]):
+            print("Interface not found... Abort?")
+            r = str(input())
+            if(r.upper() == "Y"):
+                return
+            else:
+                r = 1
+    print("Mode: ")
+    mode = str(input())
+    query = {'description': desc,
+            'interfaces': inter,
+            'mode': mode,
+            'name': name,
+            'type': 'securityzone'}
+    fmc.object.securityzone.create(data=query)
+
+def addNetObjCisco(fmc: FMC, name):
+    print("Adding Network Object with the name: "+ name)
+    print("Description: ")
+    desc = str(input())
+    print("Type (HOST/NETWORK/FQDN/RANGE): ")
+    subType = str(input())
+    print("Value (IP/IP with netmask/Domain Name/IP-IP): ")
+    ipaddr = str(input())
+    query = {"description": desc,
+            "dnsResolution": "IPV4_ONLY",
+            "name": name,
+            "subType": subType,
+            "type": "networkobject",
+            "value": ipaddr}
+    fmc.object.network.create(data=query)
+
+def addUserCisco(userset, login, passwd, ip_addr):
+    fmc = FMC(ip_addr,login,passwd,"https")
+    for x in userset:
+        user  = {'identitySourceId': x[0],
+                'name': x[1],
+                'newPassword': '',
+                'password': x[2],
+                'type': 'user',
+                'userRole': x[3],
+                'userServiceTypes': [x[4]]}
+        fmc.object.realmuser.create(data=user)
+
+def addFTDCisco(ftdSet, login, passwd, ip_addr):
+    fmc = FMC(ip_addr,login,passwd,"https")
+    for x in ftdSet:
+        accessPolicy = fmc.policy.accesspolicy.get(name=x[5])
+        licenceCaps = x[4].split()
+        ftd  = {"name": x[0],
+                "hostName": x[1],
+                "regKey": x[2],
+                "type": x[3],
+                "license_caps": licenceCaps,
+                "accessPolicy": {
+                    "id": accessPolicy["id"],
+                    "type": "AccessPolicy",
+                    "name": x[5]
+                    }
+                }
+        fmc.device.devicerecord.create(data=ftd)
 
 def getRulesCisco(login, passwd, ip_addr):
-    fmc = FMC(ip_addr, login, passwd,"Global")
-    print(fmc.policy.accesspolicy.get())            
+    fmc = FMC(ip_addr, login, passwd, "https")
+    data = fmc.policy.accesspolicy.get()[0]
+    uuidString = data["rules"]["links"]["self"]
+    print(json.dumps(fmc.policy.accesspolicy.accessrule.get(container_uuid=uuidString[uuidString.index("policies/")+9:uuidString.index("/accessrules")]),indent=4))
 
-
-def getRulesFortigate(url, api_key):
+def getPolicyCisco(login, passwd, ip_addr, containerName):
+    fmc = FMC(ip_addr, login, passwd, "https")
     try:
-        #Policies request
-        url_cust_req="http://"+url+"/api/v2/cmdb/firewall/policy?access_token="+api_key
-        r = requests.get(url=url_cust_req, headers="Authorization: Bearer "+api_key, verify=False)
-        
-        if(r == None):
-            raise Exception
-        else:
-            print(r)
-    except Exception:
-        return SESSION_ERROR
-    
+        print(json.dumps(fmc.policy.accesspolicy.get(name=containerName),indent=4))
+        return True
+    except exceptions.ResourceNotFoundError:
+        print("Policy with the name '"+containerName+"' does not exist, please add it in the FMC.")
+        return False
+def getDevicesCisco(login, passwd, ip_addr):
+    fmc = FMC(ip_addr, login, passwd, "https")
+    print(json.dumps(fmc.device.devicerecord.get(),indent=4))
+
+def getUsersCisco(login, passwd, ip_addr):
+    fmc = FMC(ip_addr, login, passwd, "https")
+    print(json.dumps(fmc.object.realmuser.get(),indent=4))      
+
+def getRulesFortigate(login, password, ip_addr):
+    device = FortiGate(ipaddr=ip_addr,username=login,password=password)
+    print(device.get_firewall_policy())
--- AutoConfigRouter/cli.py
+++ AutoConfigRouter/cli.py
@@ -15,8 +15,56 @@ from AutoConfigRouter import __app_name__, __version__, ERRORS, config
 app = typer.Typer()
 
 @app.command()
-def uploadCisco(
-    ip_address: str = typer.Option(
+def getRulesCisco(
+    ip_addr: str = typer.Option(
+        "192.168.1.1",
+        "--ip-address",
+        "-ip",
+        prompt="Set ip address:",
+    ),
+    login: str = typer.Option(
+        "admin",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "password",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+) -> None:
+    """Get the rules from a Cisco FMC."""
+    AutoConfigRouter.autoconfigrouter.getRulesCisco(login,password,ip_addr)
+
+@app.command()
+def getDevicesCisco(
+    ip_addr: str = typer.Option(
+        "192.168.1.1",
+        "--ip-address",
+        "-ip",
+        prompt="Set ip address:",
+    ),
+    login: str = typer.Option(
+        "admin",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "password",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+) -> None:
+    """Get the devices from a Cisco FMC."""
+    AutoConfigRouter.autoconfigrouter.getDevicesCisco(login,password,ip_addr)
+
+@app.command()
+def addFTDCisco(
+    ip_addr: str = typer.Option(
         "192.168.1.1",
         "--ip-address",
         "-ip",
@@ -41,7 +89,80 @@ def uploadCisco(
         prompt="Set file location:",
     ),
 ) -> None:
+    """Add the FTD device to the target FMC."""
+    wb = openpyxl.Workbook()
+    wb = load_workbook(filename = file, read_only=1)
+    ws = wb.active
+    ftdSet = []
+    ftd = []
+    for i in range(2, ws.max_row+1):
+        ftd.clear()
+        for j in range(1, ws.max_column+1):
+            data = ws.cell(row=i, column=j)
+            ftd.append(data.value)
+        ftdSet.append(ftd.copy())
+    AutoConfigRouter.autoconfigrouter.addFTDCisco(ftdSet,login,password,ip_addr)
+
+@app.command()
+def getUsersCisco(
+    ip_addr: str = typer.Option(
+        "192.168.1.1",
+        "--ip-address",
+        "-ip",
+        prompt="Set ip address:",
+    ),
+    login: str = typer.Option(
+        "cisco",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "cisco123",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+) -> None:
+    """Get the users from a Cisco FMC."""
+    AutoConfigRouter.autoconfigrouter.getUsersCisco(login,password,ip_addr)
+
+@app.command()
+def uploadRulesCisco(
+    ip_address: str = typer.Option(
+        "192.168.1.1",
+        "--ip-address",
+        "-ip",
+        prompt="Set ip address:",
+    ),
+    login: str = typer.Option(
+        "cisco",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "cisco123",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+    containerName: str = typer.Option(
+        "Test Policy",
+        "--container",
+        "-c",
+        prompt="Set Policy Name:"
+    ),
+    file: str = typer.Option(
+        r"C:\Users\User\Documents\spreadsheet.xlsx",
+        "--file",
+        "-f",
+        prompt="Set file location:",
+    ),
+) -> None:
     """Upload rulesets from an excel file to a Cisco FMC."""
+    if(AutoConfigRouter.autoconfigrouter.getPolicyCisco(login,password,ip_address,containerName) == False):
+        return
     wb = openpyxl.Workbook()
     wb = load_workbook(filename = file, read_only=1)
     ws = wb.active
@@ -51,12 +172,39 @@ def uploadCisco(
         rule.clear()
         for j in range(1, ws.max_column+1):
             data = ws.cell(row=i, column=j)
-            if(data.value != 'None'):
-                rule.append(data.value)
-            elif(data.value != 'None' and j<=ws.max_column+1):
-                rule.append(None)
+            rule.append(data.value)
         ruleset.append(rule.copy())
-    AutoConfigRouter.autoconfigrouter.addRulesCisco(ruleset,login,password,ip_address)
+    AutoConfigRouter.autoconfigrouter.addRulesCisco(ruleset,login,password,ip_address,containerName)
+
+@app.command()
+def getPolicyCisco(
+    ip_address: str = typer.Option(
+        "192.168.1.1",
+        "--ip-address",
+        "-ip",
+        prompt="Set ip address:",
+    ),
+    login: str = typer.Option(
+        "cisco",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "cisco123",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
+    ),
+    containerName: str = typer.Option(
+        "Test Policy",
+        "--container",
+        "-c",
+        prompt="Set Policy Name:"
+    ),
+) -> None:
+    """Get a container's information from a Cisco FMC."""
+    AutoConfigRouter.autoconfigrouter.getPolicyCisco(login,password,ip_address,containerName)
 
 @app.command()
 def uploadForti(
@@ -79,7 +227,7 @@ def uploadForti(
         prompt="Set file location:",
     ),
 ) -> None:
-    """Upload a rule to a Fortinet firewall."""
+    """Upload a rulebook to a Fortinet firewall."""
     wb = openpyxl.Workbook()
     wb = load_workbook(filename = file, read_only=1)
     ws = wb.active
@@ -104,15 +252,21 @@ def getForti(
         "-ip",
         prompt="Set ip address:",
     ),
-    api_key: str = typer.Option(
-        "gHmt8z6rx49Qy58Q9mkNqn377jxmH8",
-        "--key",
-        "-k",
-        prompt="Set API-User Key:",
+    login: str = typer.Option(
+        "cisco",
+        "--login",
+        "-l",
+        prompt="Set login credential:",
+    ),
+    password: str = typer.Option(
+        "cisco123",
+        "--passwd",
+        "-p",
+        prompt="Set password:",
     ),
 ) -> None:
     """Get the rules from a fortinet firewall."""
-    AutoConfigRouter.autoconfigrouter.getRulesFortigate(ip_addr,api_key)
+    AutoConfigRouter.autoconfigrouter.getRulesFortigate(login,password,ip_addr)
 
 @app.command()
 def validate(
